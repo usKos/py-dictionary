b0bb887be451f0e668c44c730828b52aca20526c
Kosta Stepanjuk <stepanjukkosta@qmail.com>
2023-10-06 23:01:55 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,2 +1,56 @@
+from typing import Any
+
+
 class Dictionary:
-    pass
+
+    def __init__(self, capacity: int = 8, load_factor: float = 2 / 3) -> None:
+        self.capacity = capacity
+        self.load_factor = load_factor
+        self.hash_table: list = [None] * capacity
+
+    def __setitem__(self, key: Any, value: Any) -> None:
+        hash_key = hash(key) % self.capacity
+        while True:
+            if self.hash_table[hash_key] is not None:
+                if self.hash_table[hash_key][0] == key:
+                    self.hash_table[hash_key][1] = value
+                    break
+                else:
+                    hash_key += 1
+            else:
+                self.hash_table[hash_key] = [key, value]
+                break
+
+        if self.__len__() > self.capacity * self.load_factor:
+            self.resize()
+
+    def __getitem__(self, key: Any) -> Any:
+        hash_key = hash(key) % self.capacity
+        while True:
+            if self.hash_table[hash_key] is not None:
+                if self.hash_table[hash_key][0] == key:
+                    return self.hash_table[hash_key][1]
+                else:
+                    hash_key += 1
+            else:
+                return None
+
+    def __len__(self) -> int:
+        return len([elem for elem in self.hash_table if elem])
+
+    def resize(self) -> None:
+        capacity2 = 2 * self.capacity
+        hash_table2 = [None] * capacity2
+
+        for elem in self.hash_table:
+            if elem is not None:
+                hash_key2 = hash(elem[0]) % capacity2
+                while True:
+                    if hash_table2[hash_key2] is None:
+                        hash_table2[hash_key2] = [elem[0], elem[1]]
+                        break
+                    else:
+                        hash_key2 += 1
+
+        self.hash_table = hash_table2
+        self.capacity = capacity2
