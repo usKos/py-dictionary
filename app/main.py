e4001739c074324d45a6c0b6e2a2b64c059f3aa9
Kosta Stepanjuk <stepanjukkosta@qmail.com>
2023-10-07 15:52:19 +0300

    'Solution_branch'

--- app/main.py
+++ app/main.py
@@ -10,30 +10,36 @@ class Dictionary:
 
     def __setitem__(self, key: Any, value: Any) -> None:
         hash_key = hash(key) % self.capacity
+
+        for i in range(len(self.hash_table)):
+            if self.hash_table[i] is not None:
+                if self.hash_table[i][0] == key:
+                    self.hash_table[i][1] = value
+                    return
+
         while True:
-            if self.hash_table[hash_key] is not None:
-                if self.hash_table[hash_key][0] == key:
-                    self.hash_table[hash_key][1] = value
-                    break
-                else:
-                    hash_key += 1
-            else:
+            if self.hash_table[hash_key] is None:
                 self.hash_table[hash_key] = [key, value]
                 break
+            else:
+                if hash_key < self.capacity - 1:
+                    hash_key += 1
+                else:
+                    hash_key = 0
 
         if self.__len__() > self.capacity * self.load_factor:
             self.resize()
 
     def __getitem__(self, key: Any) -> Any:
         hash_key = hash(key) % self.capacity
-        while True:
-            if self.hash_table[hash_key] is not None:
-                if self.hash_table[hash_key][0] == key:
-                    return self.hash_table[hash_key][1]
-                else:
-                    hash_key += 1
-            else:
-                return None
+
+        for i in range(len(self.hash_table)):
+            if self.hash_table[i] is not None:
+                hash_key_tab = hash(self.hash_table[i][0]) % self.capacity
+                if self.hash_table[i][0] == key and hash_key_tab == hash_key:
+                    return self.hash_table[i][1]
+
+        raise KeyError(f"Invalid key: {key}")
 
     def __len__(self) -> int:
         return len([elem for elem in self.hash_table if elem])
@@ -50,7 +56,10 @@ class Dictionary:
                         hash_table2[hash_key2] = [elem[0], elem[1]]
                         break
                     else:
-                        hash_key2 += 1
+                        if hash_key2 < self.capacity - 1:
+                            hash_key2 += 1
+                        else:
+                            hash_key2 = 0
 
         self.hash_table = hash_table2
         self.capacity = capacity2
